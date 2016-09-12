38ff796c00a79a2fdf5a72caa0d13308be12cc75
kenzhang <kenverygood513@gmail.com>
2016-09-11 17:38:56 -0700

    Got rid of the for loop and used the equals method to simplify the process of comparing two arrays.

--- permutation.java
+++ permutation.java
@@ -14,7 +14,7 @@ public class permutation {
 		char[] testStringArray1 = permutedString1.toCharArray();	
 		char[] testStringArray2 = permutedString2.toCharArray();
 		char[] notPermutedArray = notPermuted.toCharArray();
-		if(isPermutation(testStringArray,notPermutedArray)){
+		if(isPermutation(testStringArray,testStringArray2)){
 			System.out.print("it is permutation!");
 		}else{
 			System.out.print("it is not permutation!");
@@ -26,12 +26,18 @@ public class permutation {
 	 	if(chararray1.length != chararray2.length){return false;}
 		Arrays.sort(chararray1); // arrays.sort method nlgn time 
 		Arrays.sort(chararray2);
+		/*The java.util.Arrays.equals(char[] a, char[] a2) method
+				returns true if the two specified arrays of chars are equal to one another.Two arrays are equal
+				 if they contain the same elements in the same order.Two array references are considered equal 
+				 if both are null. */
+		return Arrays.equals(chararray1,chararray2);
 		
+		/*
 		for(int i=0; i<chararray1.length;i++){		// compare the array n time 		
 			if (chararray1[i] != chararray2[i]){
 				return false;
 			}						
 		}	 
-	 return true;
+	 return true;*/
  }
 }
